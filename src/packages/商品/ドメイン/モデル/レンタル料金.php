819b1834675a5142f64c188b4e2d61eae8a1ad33
nomura.kainos@gmail.com <nomura.kainos@gmail.com>
2020-08-19 15:31:22 +0900

    feat:ドメインオブジェクトの追加

---
+++ src/packages/商品/ドメイン/モデル/レンタル料金.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace 商品\ドメイン\モデル;
+
+use 商品\ドメイン\モデル\基盤\正数;
+
+final class レンタル料金 extends 正数
+{
+}
---
+++ src/packages/商品/ドメイン/モデル/商品ID.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace 商品\ドメイン\モデル;
+
+use 商品\ドメイン\モデル\基盤\ユニークキー;
+
+final class 商品ID extends ユニークキー
+{
+}
