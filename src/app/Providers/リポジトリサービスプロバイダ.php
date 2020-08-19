7325345dd8accaee0ed25daea7796f4a1b39a94b
nomura.kainos@gmail.com <nomura.kainos@gmail.com>
2020-08-19 11:44:43 +0900

    feat:リポジトリサービスプロバイダの作成

---
+++ src/app/Providers/リポジトリサービスプロバイダ.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use 商品\インフラ\リポジトリ\商品リポジトリ;
+use 商品\ドメイン\モデル\商品リポジトリインターフェース;
+
+class リポジトリサービスプロバイダ extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        $this->app->bind(
+            商品リポジトリインターフェース::class,
+            商品リポジトリ::class
+        );
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
+
