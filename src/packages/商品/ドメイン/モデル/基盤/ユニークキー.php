81af1c976aa7d773e5973d7bce742ef4b9654bb7
nomura.kainos@gmail.com <nomura.kainos@gmail.com>
2020-08-19 15:28:36 +0900

    feat:ドメインモデルの基盤クラスを作成 インスタンスIDとインスタンスの値が全く同じものは、1つしか作れないように制約を追加

---
+++ src/packages/商品/ドメイン/モデル/基盤/ユニークキー.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace 商品\ドメイン\モデル\基盤;
+
+class ユニークキー
+{
+    protected $値;
+
+    private function __construct(int $値)
+    {
+        $this->値 = $値;
+    }
+
+    public function 値(): int
+    {
+        return $this->値;
+    }
+
+    public function equals(self $id): bool
+    {
+        return $this->値 === $id->値;
+    }
+
+    public static function 作成(int $value)
+    {
+        // インスタンスIDとインスタンスの値が全く同じものは、1つしか作れないように
+        // staticにしている
+        return new static($value);
+    }
+}
