7bfc06b0137a43ba5882b597ba77b16070d6b637
nomura.kainos@gmail.com <nomura.kainos@gmail.com>
2020-08-22 17:27:50 +0900

    feat:バリューオブジェクトの作成

--- src/packages/商品/ドメイン/モデル/レンタル料金.php
+++ src/packages/商品/ドメイン/モデル/レンタル料金.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace 商品\ドメイン\モデル;
 
-final class レンタル料金
+use 商品\ドメイン\モデル\基盤\バリューオブジェクト;
+
+final class レンタル料金 extends バリューオブジェクト
 {
 }
---
+++ src/packages/商品/ドメイン/モデル/基盤/バリューオブジェクト.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace 商品\ドメイン\モデル\基盤;
+
+class バリューオブジェクト
+{
+    protected $値;
+
+    private function __construct(int $値)
+    {
+        $this->値 = $値;
+    }
+
+    public function 値(): int
+    {
+        return $this->値;
+    }
+
+    public function 等しいか(self $値): bool
+    {
+        return $this->値 === $値;
+    }
+
+    public static function 作成(int $値)
+    {
+        // インスタンスIDとインスタンスの値が全く同じものは、1つしか作れないように
+        // staticにしている
+        return new static($値);
+    }
+}
