affef855a056941a3141a920201849eb3924aa4e
nomura.kainos@gmail.com <nomura.kainos@gmail.com>
2020-08-22 15:24:44 +0900

    fix:不要なuseを削除

--- src/packages/商品/インフラ/エロクアント/商品エロクアント.php
+++ src/packages/商品/インフラ/エロクアント/商品エロクアント.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 namespace 商品\インフラ\エロクアント;
 
 use Illuminate\Database\Eloquent\Model;
-use 商品\ドメイン\モデル\レンタル料金;
-use 商品\ドメイン\モデル\商品;
-use 商品\ドメイン\モデル\商品ID;
 
 class 商品エロクアント extends Model
 {
