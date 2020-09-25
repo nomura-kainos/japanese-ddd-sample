58d16e57d7df9da3b07be6e25bf0071f8828c5a0
nomura.kainos@gmail.com <nomura.kainos@gmail.com>
2020-09-25 16:38:53 +0900

    refactor:日本語に変更

--- src/packages/共通/バリューオブジェクト.php
+++ src/packages/共通/バリューオブジェクト.php
@@ -13,9 +13,9 @@ trait バリューオブジェクト
      * 本来は、privateプロパティにアクセスできることは良くないが、readonlyの代用で実装が簡単な方法がなかったためtraitを使用
      * https://qiita.com/0w0/items/fa927961a54c9bb7a66b#%E9%9D%A2%E5%80%92%E3%81%AA%E3%81%AE%E3%81%A7getter%E3%82%92%E5%85%B1%E9%80%9A%E5%8C%96%E3%81%99%E3%82%8B
      */
-    public function __get($name)
+    public function __get($プロパティ名)
     {
-        return $this->$name;
+        return $this->$プロパティ名;
     }
 
     final public function 等しいか(self $バリューオブジェクト): bool
