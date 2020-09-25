840ac68b8ec221fc0bc091d7b007862aa862249d
nomura.kainos@gmail.com <nomura.kainos@gmail.com>
2020-09-25 11:52:00 +0900

    test:ユニークキーのテスト追加

---
+++ src/tests/pakages/共通/ユニークキーテスト.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\pakages\共通;
+
+use Tests\TestCase;
+use 共通\ユニークキー;
+
+class ユニークキーテスト extends TestCase
+{
+    public function test_設定した属性の値が入力と同じこと()
+    {
+        $ユニークキー = new ユニークキー(1);
+
+        self::assertSame(1, $ユニークキー->値());
+    }
+}
