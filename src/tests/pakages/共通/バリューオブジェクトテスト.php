0769a58b8fb29b55360343bbfc6e500e6fbad6a0
nomura.kainos@gmail.com <nomura.kainos@gmail.com>
2020-09-25 15:58:28 +0900

    test:プロパティに複数のバリューオブジェクトを持つテストケースを追加

--- src/tests/pakages/共通/バリューオブジェクトテスト.php
+++ src/tests/pakages/共通/バリューオブジェクトテスト.php
@@ -5,6 +5,8 @@ namespace Tests\pakages\共通;
 
 use Tests\TestCase;
 use 共通\バリューオブジェクト;
+use 商品\ドメイン\モデル\レンタル料金;
+use 商品\ドメイン\モデル\商品ID;
 
 class バリューオブジェクトテスト extends TestCase
 {
@@ -41,6 +43,15 @@ class バリューオブジェクトテスト extends TestCase
 
         self::assertSame(1, $オブジェクト->id);
     }
+
+    public function test_設定した属性の値が入力と同じこと_プロパティにバリューオブジェクトを含む場合()
+    {
+        $オブジェクト = new バリューオブジェクトを持つテストバリューオブジェクト(new 商品ID(1), '名前', new レンタル料金(2000));
+
+        self::assertSame(1, $オブジェクト->id->値);
+        self::assertSame('名前', $オブジェクト->名前);
+        self::assertSame(2000, $オブジェクト->レンタル料金->値);
+    }
 }
 /**
  * @property-read int $id
@@ -55,3 +66,24 @@ class テストバリューオブジェクト{
         $this->id = $id;
     }
 }
+
+/**
+ * @property-read 商品ID $id
+ * @property-read string $名前
+ * @property-read レンタル料金 $レンタル料金
+ */
+class バリューオブジェクトを持つテストバリューオブジェクト{
+    use バリューオブジェクト;
+
+    private 商品ID $id;
+    private string $名前;
+    private レンタル料金 $レンタル料金;
+
+    public function __construct(商品ID $id, string $名前, レンタル料金 $レンタル料金)
+    {
+        $this->id = $id;
+        $this->名前 = $名前;
+        $this->レンタル料金 = $レンタル料金;
+    }
+}
+
