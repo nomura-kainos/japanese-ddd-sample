979dbdc59b653c6931c2d734ec9d81568d6c5ae6
nomura.kainos@gmail.com <nomura.kainos@gmail.com>
2020-09-30 10:58:13 +0900

    fix:エロクアントで取得した値がすべてstringになるため、属性キャストを指定

--- src/packages/商品/インフラ/エロクアント/商品エロクアント.php
+++ src/packages/商品/インフラ/エロクアント/商品エロクアント.php
@@ -5,6 +5,9 @@ namespace 商品\インフラ\エロクアント;
 
 use Illuminate\Database\Eloquent\Model;
 
+/*
+ * https://readouble.com/laravel/5.7/ja/eloquent-mutators.html
+ */
 class 商品エロクアント extends Model
 {
 
@@ -13,6 +16,13 @@ class 商品エロクアント extends Model
 
     // INSERT時の自動採番の有無を指定する
     public $incrementing = false;
+
+    // フィールドの属性キャストを指定(未指定の場合stringになる)
+    protected $casts = [
+        'id' => 'int',
+        '名前' => 'string',
+        'レンタル料金' => 'int',
+    ];
 
     // 代入可能なフィールドを指定する
     protected $fillable = ['id', '名前', 'レンタル料金'];
