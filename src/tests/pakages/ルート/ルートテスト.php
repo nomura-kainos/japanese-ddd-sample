cdc5a3435d0815a22d118fc18fc6b24eaa9621f3
nomura.kainos@gmail.com <nomura.kainos@gmail.com>
2020-10-01 13:27:38 +0900

    test:POST専用のテストを作成

--- src/tests/pakages/ルート/ルートテスト.php
+++ src/tests/pakages/ルート/ルートテスト.php
@@ -42,6 +42,45 @@ class ルートテスト extends TestCase
         ];
     }
 
+    /**
+     * @dataProvider URIプロバイダ_POST
+     * @param string $URI
+     * @param array $入力
+     * @param string $リダイレクトURI
+     */
+    public function test_画面に遷移できること_POST(string $URI, array $入力, string $リダイレクトURI)
+    {
+        factory(商品エロクアント::class, '商品が1件登録済')->create();
+        $正常ステータス = 200;
+
+        $レスポンス = $this->post($URI, $入力);
+
+        $レスポンス->assertRedirect($リダイレクトURI);
+    }
+
+    public function URIプロバイダ_POST()
+    {
+        return [
+            [
+                '/item/edit/1',
+                [
+                    'id' => 1,
+                    '名前' => '更新',
+                    'レンタル料金' => 2000,
+                ],
+                '/item',
+            ],
+            [
+                '/item/register',
+                [
+                    '名前' => '登録',
+                    'レンタル料金' => 1000,
+                ],
+                '/item',
+            ],
+        ];
+    }
+
     public function test_存在しない画面に遷移できないこと()
     {
         $レスポンス = $this->get('/item/1');
