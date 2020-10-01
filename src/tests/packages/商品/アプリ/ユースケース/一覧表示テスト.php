08e4ceb1f3541fca478f7d442c9abd76db138ed8
nomura.kainos@gmail.com <nomura.kainos@gmail.com>
2020-10-01 15:00:47 +0900

    test:一覧表示のユースケーステストを追加

---
+++ src/tests/packages/商品/アプリ/ユースケース/一覧表示テスト.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\packages\商品\アプリ\ユースケース;
+
+use Illuminate\Support\Collection;
+use Tests\TestCase;
+use 商品\アプリ\ユースケース\一覧表示;
+use 商品\インフラ\リポジトリ\商品リポジトリ;
+use 商品\インフラ\レスポンスデータ\商品コレクションレスポンスデータ;
+use 商品\プレゼンテーション\ビューモデル\一覧ビューモデル;
+
+/**
+ * @group 商品
+ */
+class 一覧表示テスト extends TestCase
+{
+    public function test_一覧ビューモデルのインスタンスが返却されること()
+    {
+
+        $リポジトリモック = $this->mock(商品リポジトリ::class, function ($モック) {
+            $空の配列 = new Collection();
+
+            $モック->shouldReceive('全件取得')
+                ->once()
+                ->andReturn(new 商品コレクションレスポンスデータ($空の配列));
+        });
+        $ユースケース = new 一覧表示($リポジトリモック);
+
+        $ビューモデル = $ユースケース->実行();
+
+        $this->assertInstanceOf(一覧ビューモデル::class, $ビューモデル);
+    }
+}
