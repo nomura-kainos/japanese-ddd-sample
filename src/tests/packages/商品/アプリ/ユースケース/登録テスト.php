34e9aafe774e8529a72c904e38d4398dae0c7dba
nomura.kainos@gmail.com <nomura.kainos@gmail.com>
2020-10-02 09:46:04 +0900

    refactor:ファクトリをDIするように変更 new ファクトリ()がコードの内部で宣言されており、テストでファクトリのモックが作りづらいため変更

--- src/tests/packages/商品/アプリ/ユースケース/登録テスト.php
+++ src/tests/packages/商品/アプリ/ユースケース/登録テスト.php
@@ -8,23 +8,35 @@ use Illuminate\Http\Request;
 use Tests\TestCase;
 use 商品\アプリ\ユースケース\登録;
 use 商品\インフラ\リポジトリ\商品リポジトリ;
-use 商品\インフラ\レスポンスデータ\商品IDレスポンスデータ;
+use 商品\ドメイン\モデル\レンタル料金;
+use 商品\ドメイン\モデル\商品;
+use 商品\ドメイン\モデル\商品ID;
+use 商品\ドメイン\モデル\商品ファクトリ;
 
 /**
  * @group 商品
  */
 class 登録テスト extends TestCase
 {
+    private function テスト商品を作成(): 商品
+    {
+        return new 商品(
+                    new 商品ID(1),
+                    '登録',
+                    new レンタル料金(1000)
+        );
+    }
     public function test_最後のメソッドが実行されること()
     {
-        $リポジトリスパイ = $this->spy(商品リポジトリ::class, function ($モック) {
-            $モック->shouldReceive('登録用に次の商品IDを取得する')
-                ->andReturn(new 商品IDレスポンスデータ(1));
+        $リポジトリスパイ = $this->spy(商品リポジトリ::class);
+        $ファクトリモック = $this->mock(商品ファクトリ::class, function ($モック) {
+            $モック->shouldReceive('作成する')
+                ->andReturn($this->テスト商品を作成());
         });
         $リクエストモック = $this->mock(Request::class);
         $リクエストモック->名前 = '名前';
         $リクエストモック->レンタル料金 = 1000;
-        $ユースケース = new 登録($リポジトリスパイ);
+        $ユースケース = new 登録($リポジトリスパイ, $ファクトリモック);
 
         $ユースケース->実行($リクエストモック);
 
