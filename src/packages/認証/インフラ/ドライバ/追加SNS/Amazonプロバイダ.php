bda73bfef6346ab2c709a3fd96793361407a3bc9
nomura <nomura.kainos@gmail.com>
2020-11-09 14:42:12 +0900

    refactor:改行を削除

--- src/packages/認証/インフラ/ドライバ/追加SNS/Amazonプロバイダ.php
+++ src/packages/認証/インフラ/ドライバ/追加SNS/Amazonプロバイダ.php
@@ -10,7 +10,6 @@ use Laravel\Socialite\Two\User;
 
 class Amazonプロバイダ extends AbstractProvider implements ProviderInterface
 {
-
     /**
      * ユーザ情報で取得するパラメータを指定する
      * https://developer.amazon.com/ja/docs/login-with-amazon/customer-profile.html
