4032c40fc91e68da1f9ca2cf398c1126988fb4c0
nomura <nomura.kainos@gmail.com>
2020-11-11 11:57:53 +0900

    style:ログイン画面にSNSログインボタンを移動

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -11,6 +11,7 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
+                        @guest
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
@@ -39,7 +40,34 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        @endguest
+
+                        <div class="form-group row mt-2">
+                            <div class="col-md-8 offset-md-4" role="button">
+                                <a href="/user/login/google" role="button">
+                                    <img border="0" alt="Login with Google"
+                                         src="{{ asset('/img/googleログインボタン.png') }}"
+                                         width="160" height="40" />
+                                </a>
+                            </div>
+                            <div class="col-md-8 offset-md-4">
+                                <a href="/user/login/facebook" role="button">
+                                    <img border="0" alt="Login with Facebook"
+                                         src="{{ asset('/img/facebookログインボタン.png') }}"
+                                         width="156" height="35" />
+                                </a>
+                            </div>
+                            <div class="col-md-8 offset-md-4">
+                                <a href="/user/login/amazon" role="button">
+                                    <img border="0" alt="Login with Amazon"
+                                         src="https://images-na.ssl-images-amazon.com/images/G/01/lwa/btnLWA_gold_156x32.png"
+                                         width="156" height="32" />
+                                </a>
+                            </div>
+                        </div>
+
+                        @guest
+                            <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
@@ -51,6 +79,8 @@
                                     </a>
                                 @endif
                             </div>
+                        @endguest
+
                         </div>
                     </form>
                 </div>
