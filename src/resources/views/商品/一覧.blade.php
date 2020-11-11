b720bd1bee6f4a81d58c0badb4b2f50bf6f97afb
nomura <nomura.kainos@gmail.com>
2020-11-11 11:35:05 +0900

    style:SNSログインのロゴ追加

--- src/resources/views/商品/一覧.blade.php
+++ src/resources/views/商品/一覧.blade.php
@@ -23,19 +23,25 @@
                 <br>
                 <a href="/item/register/"> 登録 </a>
                 <div class="form-group row mt-2">
-                    <div class="col-md-8 offset-md-4">
-                        <a href="/user/login/google" class="btn btn-secondary" role="button">
-                            Google Login
+                    <div class="col-md-8 offset-md-4" role="button">
+                        <a href="/user/login/google" role="button">
+                            <img border="0" alt="Login with Google"
+                                 src="{{ asset('/img/googleログインボタン.png') }}"
+                                 width="160" height="40" />
                         </a>
                     </div>
                     <div class="col-md-8 offset-md-4">
-                        <a href="/user/login/facebook" class="btn btn-secondary" role="button">
-                            Facebook Login
+                        <a href="/user/login/facebook" role="button">
+                            <img border="0" alt="Login with Google"
+                                 src="{{ asset('/img/facebookログインボタン.png') }}"
+                                 width="156" height="35" />
                         </a>
                     </div>
                     <div class="col-md-8 offset-md-4">
-                        <a href="/user/login/amazon" class="btn btn-secondary" role="button">
-                            Amazon Login
+                        <a href="/user/login/amazon" role="button">
+                            <img border="0" alt="Login with Amazon"
+                                 src="https://images-na.ssl-images-amazon.com/images/G/01/lwa/btnLWA_gold_156x32.png"
+                                 width="156" height="32" />
                         </a>
                     </div>
                 </div>
