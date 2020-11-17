f3aa8adbe4bd6e065e06e0c7b6d5b8d30db81b40
nomura <nomura.kainos@gmail.com>
2020-11-17 14:42:34 +0900

    refactor:テーブルのカラムと同じ名前に修正

--- src/packages/カート/ドメイン/モデル/カート内商品.php
+++ src/packages/カート/ドメイン/モデル/カート内商品.php
@@ -10,23 +10,23 @@ use 共通\ユニークキー;
 class カート内商品 extends エンティティ
 {
 
-    private カートID $id;
+    private カートID $カートid;
     private カート内商品ID $商品id;
     private int $数量;
     private bool $注文済みか;
 
-    public function __construct(カートID $id, カート内商品ID $商品id, int $数量, bool $注文済みか)
+    public function __construct(カートID $カートid, カート内商品ID $商品id, int $数量, bool $注文済みか)
     {
-        parent::ユニークキーを設定する(new ユニークキー($id->値 . $商品id->値));
-        $this->id = $id;
+        parent::ユニークキーを設定する(new ユニークキー($カートid->値 . $商品id->値));
+        $this->カートid = $カートid;
         $this->商品id = $商品id;
         $this->数量 = $数量;
         $this->注文済みか = $注文済みか;
     }
 
-    public function id(): int
+    public function カートid(): int
     {
-        return $this->id->値;
+        return $this->カートid->値;
     }
 
     public function 商品id(): int
