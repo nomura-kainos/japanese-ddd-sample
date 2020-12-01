dd147b1113338771b5fbbdc02cf393655f414d73
nomura <nomura.kainos@gmail.com>
2020-12-01 16:52:49 +0900

    refactor:シーケンスを導入したため、商品登録前にシーケンスのidを登録する

--- src/database/seeds/商品が1件登録済シーダ.php
+++ src/database/seeds/商品が1件登録済シーダ.php
@@ -13,6 +13,10 @@ class 商品が1件登録済シーダ extends Seeder
         DB::table($テーブル名)->delete();
 
         // データ挿入
+        DB::table($テーブル名 . 'シーケンス')->insert([
+            'id' => 1,
+        ]);
+
         DB::table($テーブル名)->insert([
             'id' => 1,
             '名前' => '登録済',
