8e6280dfb570f410086cc3b2dd92db8874cb1b11
nomura <nomura.kainos@gmail.com>
2020-12-04 16:46:35 +0900

    style:ダブルクオートに書式を統一 調べた結果、htmlの属性値はダブルクオートの方が一般的なため

--- src/resources/views/特集/一覧.blade.php
+++ src/resources/views/特集/一覧.blade.php
@@ -1,21 +1,21 @@
-@extends('layouts.app')
+@extends("layouts.app")
 
-@section('content')
-<div class='container'>
-    <div class='row justify-content-center'>
-        <div class='col-md-8'>
-            <div class='card'>
-                <div class='card-header'>特集一覧</div>
+@section("content")
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">特集一覧</div>
 
                 <table>
                     @foreach ($複数特集->取得() as $特集)
                     <tr>
-                        <td><a href='/report_detail/{{ $特集->id() }}'> {{ $特集->タイトル() }} </a></td>
+                        <td><a href="/report_detail/{{ $特集->id() }}"> {{ $特集->タイトル() }} </a></td>
                     </tr>
                     @endforeach
                 </table>
                 <br>
-                <a href='/report/register/'> 登録 </a>
+                <a href="/report/register/"> 登録 </a>
             </div>
         </div>
     </div>
--- src/resources/views/特集/登録.blade.php
+++ src/resources/views/特集/登録.blade.php
@@ -1,23 +1,23 @@
 <head>
     <!-- include libraries(jQuery, bootstrap) -->
-    <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'>
-    <script src='https://code.jquery.com/jquery-3.5.1.slim.min.js'></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 
-    <script src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'></script>
-    <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js'></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 </head>
 
 <body>
-<div class='container'>
-    <div class='row'>
-        <div class='col-md-7 offset-3 mt-4'>
-            <div class='card-body'>
+<div class="container">
+    <div class="row">
+        <div class="col-md-7 offset-3 mt-4">
+            <div class="card-body">
 
-                <form action='{{ url('/report/register')}}' method='POST'>
+                <form action="{{ url("/report/register")}}" method="POST">
                     {{ csrf_field() }}
 
                     @if ($errors->any())
-                        <div class='alert alert-danger'>
+                        <div class="alert alert-danger">
                             <ul>
                                 @foreach ($errors->all() as $error)
                                     <li>{{ $error }}</li>
@@ -26,9 +26,9 @@
                         </div>
                     @endif
 
-                    タイトル：<input type='text' name='タイトル'><br>
-                    本文：<textarea id='summernote' name='本文'></textarea>
-                    <div><input type='submit' value='登録'></div>
+                    タイトル：<input type="text" name="タイトル"><br>
+                    本文：<textarea id="summernote" name="本文"></textarea>
+                    <div><input type="submit" value="登録"></div>
                 </form>
 
             </div>
@@ -38,25 +38,25 @@
 </body>
 
 <!-- summernote css/js -->
-<link href='https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css' rel='stylesheet'>
-<script src='https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js'></script>
-<script src='https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/lang/summernote-ja-JP.js'></script><!-- 日本語対応 -->
-<script type='text/javascript'>
-    $('#summernote').summernote({
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/lang/summernote-ja-JP.js"></script><!-- 日本語対応 -->
+<script type="text/javascript">
+    $("#summernote").summernote({
         height: 900,
         width: 800,
-        lang: 'ja-JP',
-        fontNames: ['YuGothic','Yu Gothic','Hiragino Kaku Gothic Pro','Meiryo','sans-serif'],
+        lang: "ja-JP",
+        fontNames: ["YuGothic","Yu Gothic","Hiragino Kaku Gothic Pro","Meiryo","sans-serif"],
         toolbar: [
-            ['style', ['style']],
-            ['font', ['bold', 'italic', 'underline', 'clear']],
-            ['fontname', ['fontname']],
-            ['fontsize', ['fontsize']],
-            ['color', ['color']],
-            ['para', ['ul', 'ol', 'paragraph']],
-            ['table', ['table']],
-            ['insert', ['link', 'picture', 'hr', 'video']],
-            ['view', ['fullscreen', 'codeview', 'help']],
+            ["style", ["style"]],
+            ["font", ["bold", "italic", "underline", "clear"]],
+            ["fontname", ["fontname"]],
+            ["fontsize", ["fontsize"]],
+            ["color", ["color"]],
+            ["para", ["ul", "ol", "paragraph"]],
+            ["table", ["table"]],
+            ["insert", ["link", "picture", "hr", "video"]],
+            ["view", ["fullscreen", "codeview", "help"]],
         ],
     });
 </script>
