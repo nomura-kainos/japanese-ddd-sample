1c21266aee8c4216956c93a4b3bd87c8e5445c72
nomura <nomura.kainos@gmail.com>
2020-12-07 10:37:40 +0900

    refactor:他の領域でも画像アップロードが使えるように別名に変更

--- src/app/Providers/ファイルサービスプロバイダ.php
+++ src/app/Providers/ファイルサービスプロバイダ.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use 商品\インフラ\アップロード\画像アップローダ;
-use 商品\ドメイン\モデル\アップロード\画像アップローダインターフェース;
+use 商品\インフラ\アップロード\画像アップローダ as 商品画像アップローダ;
+use 商品\ドメイン\モデル\アップロード\画像アップローダインターフェース as 商品画像アップローダインターフェース;
 
 class ファイルサービスプロバイダ extends ServiceProvider
 {
@@ -18,8 +18,8 @@ class ファイルサービスプロバイダ extends ServiceProvider
     public function register()
     {
         $this->app->bind(
-            画像アップローダインターフェース::class,
-            画像アップローダ::class
+            商品画像アップローダインターフェース::class,
+            商品画像アップローダ::class
         );
     }
 
