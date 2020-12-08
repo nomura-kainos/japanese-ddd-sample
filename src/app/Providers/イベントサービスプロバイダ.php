71e25af420e039f244df6a1d23fa5b5d1b5dc1dd
nomura <nomura.kainos@gmail.com>
2020-12-08 13:58:43 +0900

    refactor:公式ドキュメントのリンクを追加

--- src/app/Providers/イベントサービスプロバイダ.php
+++ src/app/Providers/イベントサービスプロバイダ.php
@@ -31,6 +31,7 @@ class イベントサービスプロバイダ extends ServiceProvider
      */
     public function boot()
     {
+        // https://readouble.com/laravel/5.8/ja/queues.html
         $this->イベントが開始されたときに、履歴が登録されるように設定する();
         $this->イベントが失敗したときに、履歴が登録されるように設定する();
         $this->イベントが成功したときに、履歴が登録されるように設定する();
