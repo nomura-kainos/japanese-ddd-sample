ad5d6e9e35b679bdb05ca06ccecf96250a1ae69d
nomura <nomura.kainos@gmail.com>
2020-12-09 09:26:48 +0900

    refactor:イベントの登録処理をリポジトリに移動

---
+++ src/packages/共通/イベント履歴リポジトリ.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace 共通;
+
+use ReflectionClass;
+
+class イベント履歴リポジトリ
+{
+    private $ジョブid;
+    private $クラス名;
+
+    public function __construct(int $ジョブid, object $クラス) {
+        $this->ジョブid = $ジョブid;
+        $this->クラス名 = $this->クラス名の取得($クラス);
+    }
+
+    private function クラス名の取得(object $クラス): string
+    {
+        return (new ReflectionClass($クラス))->getShortName();
+    }
+
+    public function 登録する(string $処理ステータス)
+    {
+        $イベント履歴 = new イベント履歴エロクアント();
+        $イベント履歴->fill(
+            [
+                'イベントid' => $this->ジョブid,
+                'イベント名' => $this->クラス名,
+                '処理ステータス' => $処理ステータス,
+            ]
+        );
+        $イベント履歴->save();
+    }
+}
