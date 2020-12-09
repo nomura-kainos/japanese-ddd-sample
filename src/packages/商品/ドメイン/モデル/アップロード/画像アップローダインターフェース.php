d146569bff9b86e1a865f285e91cf2432bd515b5
nomura <nomura.kainos@gmail.com>
2020-12-09 10:56:04 +0900

    refactor:使用していない変数を削除

--- src/packages/商品/ドメイン/モデル/アップロード/画像アップローダインターフェース.php
+++ src/packages/商品/ドメイン/モデル/アップロード/画像アップローダインターフェース.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace 商品\ドメイン\モデル\アップロード;
 
-use 商品\ドメイン\モデル\商品ID;
-
 interface 画像アップローダインターフェース
 {
-    public function ストレージに送信する(ファイル $画像ファイル, 商品ID $商品id): ?ファイル;
+    public function ストレージに送信する(ファイル $画像ファイル): ?ファイル;
 }
