2cdd497c717ef819c78ed1e1c5a57b593a5a8123
nomura <nomura.kainos@gmail.com>
2020-12-09 09:30:06 +0900

    refactor:未処理のイベント履歴を削除 未処理のイベントはイベントテーブルに残るため、イベント履歴に入れなくても確認できる

--- src/packages/共通/ドメインイベント.php
+++ src/packages/共通/ドメインイベント.php
@@ -11,21 +11,8 @@ class ドメインイベント extends Event
 {
     use SerializesModels;
 
-    private function イベント履歴の登録()
-    {
-        $イベント履歴 = new イベント履歴エロクアント();
-        $イベント履歴->fill(
-            [
-                'イベントid' => -1,//キューに入る前のidがないため、採番されない番号を設定する
-                '処理ステータス' => '未処理',
-            ]
-        );
-        $イベント履歴->save();
-    }
-
     public function 実行()
     {
-        $this->イベント履歴の登録();
         Event::dispatch($this);
     }
 }
