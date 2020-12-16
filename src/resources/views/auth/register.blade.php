cb71d99e045716fa0df5a53418432f39d62b509d
nomura <nomura.kainos@gmail.com>
2020-12-16 17:03:08 +0900

    refactor:ユーザ登録画面からSNSログインできるようにリンクを追加

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -47,6 +47,30 @@
                             </div>
                         </div>
 
+                        <div class="form-group row mt-2">
+                            <div class="col-md-8 offset-md-4" role="button">
+                                <a href="/user/login/google" role="button">
+                                    <img border="0" alt="Login with Google"
+                                         src="{{ asset('/img/googleログインボタン.png') }}"
+                                         width="160" height="40" />
+                                </a>
+                            </div>
+                            <div class="col-md-8 offset-md-4">
+                                <a href="/user/login/facebook" role="button">
+                                    <img border="0" alt="Login with Facebook"
+                                         src="{{ asset('/img/facebookログインボタン.png') }}"
+                                         width="156" height="35" />
+                                </a>
+                            </div>
+                            <div class="col-md-8 offset-md-4">
+                                <a href="/user/login/amazon" role="button">
+                                    <img border="0" alt="Login with Amazon"
+                                         src="https://images-na.ssl-images-amazon.com/images/G/01/lwa/btnLWA_gold_156x32.png"
+                                         width="156" height="32" />
+                                </a>
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
