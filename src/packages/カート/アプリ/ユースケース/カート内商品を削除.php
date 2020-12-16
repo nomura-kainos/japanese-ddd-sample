d39007fe01b8d1f8f016ebee7ea7fd94f29b95d2
nomura <nomura.kainos@gmail.com>
2020-12-16 11:20:40 +0900

    refactor:ドメインサービスを削除 ドメインサービスはドメインが持つべきロジックを書かずに、散在させる原因になるため 可能か限り使いたくないため

--- src/packages/カート/アプリ/ユースケース/カート内商品を削除.php
+++ src/packages/カート/アプリ/ユースケース/カート内商品を削除.php
@@ -4,19 +4,22 @@ declare(strict_types=1);
 
 namespace カート\アプリ\ユースケース;
 
-use カート\ドメイン\モデル\カートドメインサービス;
+use カート\ドメイン\モデル\カートID;
+use カート\ドメイン\モデル\カートリポジトリインターフェース;
+use カート\ドメイン\モデル\カート内商品ID;
 
 class カート内商品を削除
 {
-    private $カートドメインサービス;
+    private $カートリポ;
 
-    public function __construct(カートドメインサービス $カートドメインサービス)
-    {
-        $this->カートドメインサービス = $カートドメインサービス;
+    public function __construct(
+        カートリポジトリインターフェース $カートリポ
+    ) {
+        $this->カートリポ = $カートリポ;
     }
 
     public function 実行(int $カートid, int $商品id)
     {
-        $this->カートドメインサービス->カート内商品を削除($カートid, $商品id);
+        $this->カートリポ->カート内商品を削除(new カートID($カートid), new カート内商品ID($商品id));
     }
 }
