696693f716fe478698aa8feecffda27bd953e590
nomura <nomura.kainos@gmail.com>
2020-12-17 09:04:53 +0900

    refactor:スペースの追加

--- src/packages/共通/エロクアント拡張/複合主キー設定.php
+++ src/packages/共通/エロクアント拡張/複合主キー設定.php
@@ -18,11 +18,11 @@ trait 複合主キー設定
     protected function setKeysForSaveQuery(Builder $query)
     {
         $keys = $this->getKeyName();
-        if(!is_array($keys)){
+        if (!is_array($keys)) {
             return parent::setKeysForSaveQuery($query);
         }
 
-        foreach($keys as $keyName){
+        foreach ($keys as $keyName) {
             $query->where($keyName, '=', $this->getKeyForSaveQuery($keyName));
         }
 
@@ -34,7 +34,7 @@ trait 複合主キー設定
      */
     protected function getKeyForSaveQuery($keyName = null)
     {
-        if(is_null($keyName)){
+        if (is_null($keyName)) {
             $keyName = $this->getKeyName();
         }
 
