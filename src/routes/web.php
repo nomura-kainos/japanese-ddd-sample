de6c80403486682a954eaeb666cb19cdbedb60f4
nomura <nomura.kainos@gmail.com>
2020-12-24 17:19:15 +0900

    refactor:不要なルートを削除

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,8 +11,6 @@
 |
 */
 
-use App\Mail\SampleMail;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 use カート\プレゼンテーション\コントローラ\カートに入れるコントローラ;
 use カート\プレゼンテーション\コントローラ\カート内商品削除コントローラ;
@@ -90,31 +88,3 @@ Route::get('/cart/remove', function () {
 });
 Route::post('/cart/remove', カート内商品削除コントローラ::class);
 
-Route::post('/cart/complete', 'App\Http\Controllers\カートコントローラ@complete')->name('cart.complete');
-
-Route::get('/hello/', 'HelloController@index')->name('hello');
-Route::post('/hello/other', 'HelloController@other');
-Route::get('/sample', 'Sample\SampleController@index')->name('sample');
-
-Route::get('hello/auth', 'HelloController@getAuth');
-Route::post('hello/auth', 'HelloController@postAuth');
-Route::get('hello/session', 'HelloController@ses_get');
-Route::post('hello/session', 'HelloController@ses_put');
-Route::get('person', 'PersonController@index');
-Route::get('person/find', 'PersonController@find');
-Route::post('person/find', 'PersonController@search');
-Route::get('person/add', 'PersonController@add');
-Route::post('person/add', 'PersonController@create');
-Route::get('person/edit', 'PersonController@edit');
-Route::post('person/edit', 'PersonController@update');
-Route::get('person/del', 'PersonController@delete');
-Route::post('person/del', 'PersonController@remove');
-Route::get('board', 'BoardController@index');
-Route::get('board/add', 'BoardController@add');
-Route::post('board/add', 'BoardController@create');
-Route::resource('rest', 'RestappController');
-
-Route::get('/sample_mail', function () {
-    Mail::to('sample@example.com')->send(new SampleMail);
-    return '送信完了!';
-});
