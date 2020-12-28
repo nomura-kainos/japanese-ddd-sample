9c152e8faf7c9d0ca3e110fafe2aeaa3e20f2cc2
nomura <nomura.kainos@gmail.com>
2020-12-28 16:45:06 +0900

    fix:大カテゴリidと一致しないカテゴリの最新日付も基準に含まれてしまうバグを修正 また最新日付よりも小カテゴリidの方が、仕様名と意図が一致するため抽出対象を変更

--- src/packages/商品カテゴリ/ドメイン/モデル/小カテゴリ/最後尾小カテゴリ仕様.php
+++ src/packages/商品カテゴリ/ドメイン/モデル/小カテゴリ/最後尾小カテゴリ仕様.php
@@ -10,7 +10,8 @@ use 商品カテゴリ\ドメイン\モデル\商品カテゴリID;
 class 最後尾小カテゴリ仕様 implements 選択
 {
     private $大カテゴリid;
-    private $最新日付;
+    private $最後尾小カテゴリid;
+    private $処理を終了するか = false;
 
     public function __construct(
         商品カテゴリID $大カテゴリid
@@ -20,16 +21,50 @@ class 最後尾小カテゴリ仕様 implements 選択
 
     public function 基準を設定する($複数カテゴリ)
     {
-        $this->最新日付 = max(array_column($複数カテゴリ, 'created_at'));
+        $抽出対象 = $複数カテゴリ;
+
+        $複数小カテゴリ = $this->大カテゴリidが一致する小カテゴリのみ抽出する($抽出対象);
+        if(empty($複数小カテゴリ))
+        {
+            $this->処理を終了するか = true;
+            return;
+        }
+        $複数小カテゴリid = $this->小カテゴリidのみ抽出する($複数小カテゴリ);
+        $最後尾小カテゴリid = $this->最後尾の小カテゴリidを抽出する($複数小カテゴリid);
+
+        $this->最後尾小カテゴリid = $最後尾小カテゴリid;
+    }
+
+    private function 大カテゴリidが一致する小カテゴリのみ抽出する(array $複数小カテゴリ): array
+    {
+        return array_filter($複数小カテゴリ, function ($小カテゴリ) {
+            return $小カテゴリ['大カテゴリid'] === $this->大カテゴリid->値;
+        });
+    }
+
+    private function 小カテゴリidのみ抽出する(array $複数小カテゴリid): array
+    {
+        return array_column($複数小カテゴリid, '小カテゴリid');
+    }
+
+    private function 最後尾の小カテゴリidを抽出する(array $小カテゴリidリスト): int
+    {
+        return max($小カテゴリidリスト);;
     }
 
     public function 満たすか($小カテゴリ): bool
     {
-        if ($this->大カテゴリid->値 != $小カテゴリ->大カテゴリid) {
+        if($this->処理を終了するか){
             return false;
         }
 
-        if ($this->最新日付 != $小カテゴリ->created_at) {
+        if($this->大カテゴリid->値 !== $小カテゴリ->大カテゴリid)
+        {
+            return false;
+        }
+
+        if($this->最後尾小カテゴリid !== $小カテゴリ->小カテゴリid)
+        {
             return false;
         }
 
