55c1a0ca761571aa4957d984fba3fd5edd2da4aa
nomura <nomura.kainos@gmail.com>
2020-12-29 11:00:38 +0900

    fix:ユーザidと一致しないカートの最新日付も基準に含まれてしまうバグを修正

--- src/packages/カート/ドメイン/モデル/最新カート仕様.php
+++ src/packages/カート/ドメイン/モデル/最新カート仕様.php
@@ -19,11 +19,43 @@ class 最新カート仕様 implements 選択
 
     public function 基準を設定する($複数カート)
     {
-        $this->最新日付 = max(array_column($複数カート, 'created_at'));
+        $抽出対象 = $複数カート;
+
+        $複数カート = $this->ユーザidが一致するカートのみ抽出する($抽出対象);
+        if(empty($複数カート))
+        {
+            $this->処理を終了するか = true;
+            return;
+        }
+        $複数作成日時 = $this->作成日時のみ抽出する($複数カート);
+        $最新の作成日時 = $this->最新の作成日時を抽出する($複数作成日時);
+
+        $this->最新日時 = $最新の作成日時;
+    }
+
+    private function ユーザidが一致するカートのみ抽出する(array $複数カート): array
+    {
+        return array_filter($複数カート, function ($カート) {
+            return $カート['ユーザid'] === $this->ユーザid->値;
+        });
+    }
+
+    private function 作成日時のみ抽出する(array $複数カート): array
+    {
+        return array_column($複数カート, 'created_at');
+    }
+
+    private function 最新の作成日時を抽出する(array $複数作成日時): string
+    {
+        return max($複数作成日時);
     }
 
     public function 満たすか($カート): bool
     {
+        if($this->処理を終了するか){
+            return false;
+        }
+
         if ($this->ユーザid->値 != $カート->ユーザid) {
             return false;
         }
