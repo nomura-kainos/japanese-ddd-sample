e4865fdcacfd07cba0e361b2e917700a67eadfa3
nomura <nomura.kainos@gmail.com>
2021-01-07 11:00:16 +0900

    refactor:意図がよりわかりやすいメソッド名に変更 アップロードすることは具体的には、ストレージにファイルを送ることのため

--- src/packages/特集/ドメイン/モデル/アップロード/画像アップローダインターフェース.php
+++ src/packages/特集/ドメイン/モデル/アップロード/画像アップローダインターフェース.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace 特集\ドメイン\モデル\アップロード;
 
-use 特集\ドメイン\モデル\特集ID;
-
 interface 画像アップローダインターフェース
 {
-    public function アップロードする(ファイル $画像ファイル, 特集ID $特集id): ?ファイル;
+    public function ストレージに送信する(ファイル $画像ファイル): ?ファイル;
 }
