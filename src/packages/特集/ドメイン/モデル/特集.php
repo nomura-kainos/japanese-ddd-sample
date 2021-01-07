bf2a4df13fcb979789f578ac93d1dba2fa5c8213
nomura <nomura.kainos@gmail.com>
2021-01-07 10:59:06 +0900

    refactor:整合性のルールを守らせるために、特集ドメインの集約ルートからしかタイトル画像を保存できないように変更する https://github.com/little-hands/ddd-q-and-a/issues/276

--- src/packages/特集/ドメイン/モデル/特集.php
+++ src/packages/特集/ドメイン/モデル/特集.php
@@ -6,12 +6,15 @@ namespace 特集\ドメイン\モデル;
 
 use 共通\エンティティ;
 use 共通\集約ルート;
+use 特集\ドメイン\モデル\アップロード\ファイル;
+use 特集\ドメイン\モデル\アップロード\画像アップローダインターフェース;
 
 class 特集 extends エンティティ implements 集約ルート
 {
     private 特集ID $id;
     private string $タイトル;
     private string $本文;
+    private ファイル $アップロード済みファイル;
 
     public function __construct(特集ID $id, string $タイトル, string $本文)
     {
@@ -34,5 +37,15 @@ class 特集 extends エンティティ implements 集約ルート
     public function 本文(): string
     {
         return $this->本文;
+    }
+
+    public function アップロード済みファイル()
+    {
+        return $this->アップロード済みファイル;
+    }
+
+    public function 画像ファイルをストレージに保存する(画像アップローダインターフェース $画像アップローダ, $画像ファイル)
+    {
+        $this->アップロード済みファイル = $画像アップローダ->ストレージに送信する(new ファイル($画像ファイル));
     }
 }
