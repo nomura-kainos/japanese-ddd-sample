e93d507a511b0d1870d4d04b506712d6f2edb7c8
nomura <nomura.kainos@gmail.com>
2021-01-15 11:08:13 +0900

    refactor:クエリサービスを作成 DBから2テーブル同時に取得した処理をクエリサービスに移動

--- src/packages/認証/ドメイン/モデル/SNSアカウント紐付け済み仕様.php
+++ src/packages/認証/ドメイン/モデル/SNSアカウント紐付け済み仕様.php
@@ -9,13 +9,13 @@ use 共通\仕様\検証;
 class SNSアカウント紐付け済み仕様 implements 検証
 {
     public function __construct(
-        private ユーザリポジトリインターフェース $ユーザリポ
+        private 登録済みユーザクエリサービスインターフェース $登録済みユーザクエリ,
     ) {
     }
 
     public function 満たすか？($SNSアカウント): bool
     {
-        $登録済みユーザ = $this->ユーザリポ->登録済みユーザを1件取得($SNSアカウント->SNS名(), $SNSアカウント->id());
+        $登録済みユーザ = $this->登録済みユーザクエリ->取得($SNSアカウント->SNS名(), $SNSアカウント->id());
         if ($登録済みユーザ !== null) {
             return true;
         }
